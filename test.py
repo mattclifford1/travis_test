dddea1d968036507fa4eb04dd508e525f078a139
mattclifford1 <mattc863@hotmail.com>
2018-09-04 15:33:50 +0100

    test file added

---
+++ test.py
@@ -0,0 +1,13 @@
+def inc(x):
+    return x + 1
+
+def times(x, y):
+    return x*y
+
+def test_this():
+    assert times(1, 2) == 2
+    assert times(1, 4) == 4
+    assert times(1, 4) == 5
+    assert inc(1) == 2
+    assert inc(1) == 3
+    
