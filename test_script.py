4ebbdb45830852de9b62ed4d0dfba005a60955fe
mattclifford1 <mattc863@hotmail.com>
2018-09-22 12:36:40 +0100

    pylint added to travis CI

--- test_script.py
+++ test_script.py
@@ -1,15 +1,26 @@
-def times(x, y):
-    return x*y
+''' script for testing purposes '''
+
+
+def times(x, y_2):
+    ''' times together '''
+    return x_1*y_2
+
 
 def test_pass():
+    ''' designed to pass '''
     assert times(1, 2) == 2
 
+
 # def test_fail():
-# 	assert times(1, 2) == 3
+    ''' designed to fail '''
+#   assert times(1, 2) == 3
+
 
 def main():
-	test_pass()
+    ''' run fucntions '''
+    test_pass()
 
-#test coverage
+
+# test coverage
 if __name__ == '__main__':
-	main()
+    main()
