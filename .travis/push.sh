71e13e0e48394b3097ebca066c10472bbb80bd5b
mattclifford1 <mattc863@hotmail.com>
2018-09-26 16:59:51 +0100

    testing

---
+++ .travis/push.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_files() {
+  # git checkout -b gh-pages
+  git add test.npy
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  git remote add origin-pages https://${GH_TOKEN}@github.com/mattclifford1/travis_test.git
+  git push origin master 
+}
+
+setup_git
+commit_files
+upload_files
